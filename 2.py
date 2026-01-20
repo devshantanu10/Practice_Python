d2750c99cf8191c9a3182aca75758a3beeb47cf5
devshantanu10 <hereshantanu10@gmail.com>
2026-01-20 22:14:05 +0545

    for loop added

--- 2.py
+++ 2.py
@@ -1,3 +1,14 @@
-numbers = [10 , 20 , 30, 40 ,50]
-total = sum(numbers)
-print("The sum of the list is:" , total)
+# numbers = [10 , 20 , 30, 40 ,50]
+# total = sum(numbers)
+# print("The sum of the list is:" , total)
+
+# from for loop methods
+
+
+numbers = [10,20,30,40,50]
+total = 0
+
+for num in numbers:
+    total += num
+
+print("The sum of numbers of list:" , total)
